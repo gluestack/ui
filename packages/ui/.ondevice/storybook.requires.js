96c235ee514645efe994f2c98212ba1c31bacd0b
ankit-tailor <tailorankit3345@gmail.com>
2022-12-21 16:58:24 +0530

    fix: remove textarea

--- packages/ui/.ondevice/storybook.requires.js
+++ packages/ui/.ondevice/storybook.requires.js
@@ -55,6 +55,7 @@ const getStories = () => {
     require('../components/Actionsheet/Actionsheet.stories.tsx'),
     require('../components/Toast/Toast.stories.tsx'),
     require('../components/Link/Link.stories.tsx'),
+    // require('../components/TextArea/TextArea.stories.tsx'),
 
     // require('../components/Icon/Icon.stories.tsx'),
 
