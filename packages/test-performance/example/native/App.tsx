c6be8c4e0a35d1207f1ef592cb50039ff502715b
Meenu Makkar <makkarmini007@gmail.com>
2023-01-03 12:39:31 +0530

    feat: performance test app created

---
+++ packages/test-performance/example/native/App.tsx
@@ -0,0 +1,13 @@
+// import { StatusBar } from "expo-status-bar";
+import { Text, View } from 'react-native';
+import React from 'react';
+// import { Button } from "ui";
+// import { ButtonExpo } from "./button-expo";
+
+export default function App() {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>Hello world!</Text>
+    </View>
+  );
+}
