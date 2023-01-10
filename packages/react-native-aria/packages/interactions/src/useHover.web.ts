461a69275c566628daba0705958476c5c7294d58
vidhi499 <katariavidhi99@gmail.com>
2023-01-10 18:32:06 +0530

    feat: react-native aria added

---
+++ packages/react-native-aria/packages/interactions/src/useHover.web.ts
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import { useHover as useHoverWeb, HoverProps } from '@react-aria/interactions';
+import { useEffect } from 'react';
+import { attachEventHandlersOnRef } from '@react-native-aria/utils';
+
+export const useHover = (props: HoverProps, ref: any) => {
+  let params = useHoverWeb(props);
+  useEffect(() => {
+    attachEventHandlersOnRef(params.hoverProps, ref);
+  }, [params, ref]);
+
+  return params;
+};
